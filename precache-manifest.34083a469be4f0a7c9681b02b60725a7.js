791d5d6dd291a99eef39d7a48767a4e2586bdd26
Alejandro Bryan <alejandrobryan023@gmail.com>
2023-04-04 03:30:20 +0200

    Updates

---
+++ precache-manifest.34083a469be4f0a7c9681b02b60725a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "724d2c83225203fdc91f",
+    "url": "/AlejandroBryan/meet.git/static/css/main.71d0e1ee.chunk.css"
+  },
+  {
+    "revision": "724d2c83225203fdc91f",
+    "url": "/AlejandroBryan/meet.git/static/js/main.724d2c83.chunk.js"
+  },
+  {
+    "revision": "0fc11f7760bb74a1f242",
+    "url": "/AlejandroBryan/meet.git/static/js/1.0fc11f77.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/AlejandroBryan/meet.git/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "163291bdfaad6db548d6",
+    "url": "/AlejandroBryan/meet.git/static/js/runtime~main.163291bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AlejandroBryan/meet.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8335452fcd4cd4ccce3340e2a3a5c4a4",
+    "url": "/AlejandroBryan/meet.git/index.html"
+  }
+];
