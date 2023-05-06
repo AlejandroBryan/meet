d7eea59dcc92895642f987bf2de8e31341d172b0
AlejandroBryan <alexbryan@pop-os.localdomain>
2023-05-06 06:55:51 +0200

    Updates

---
+++ precache-manifest.35bc95ada44d04ea1547db2c5df5d2d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "afff8692565ba7935e67",
+    "url": "/meet/static/css/main.7bcd9bfd.chunk.css"
+  },
+  {
+    "revision": "afff8692565ba7935e67",
+    "url": "/meet/static/js/main.afff8692.chunk.js"
+  },
+  {
+    "revision": "2cdaf3eea228255e78d1",
+    "url": "/meet/static/js/1.2cdaf3ee.chunk.js"
+  },
+  {
+    "revision": "209dd765f9f2d1da6c4d",
+    "url": "/meet/static/css/2.12e4ab67.chunk.css"
+  },
+  {
+    "revision": "209dd765f9f2d1da6c4d",
+    "url": "/meet/static/js/2.209dd765.chunk.js"
+  },
+  {
+    "revision": "0c9a8aed75fab719dc1e",
+    "url": "/meet/static/js/runtime~main.0c9a8aed.js"
+  },
+  {
+    "revision": "87847334c3d6236320ca3890dd9db908",
+    "url": "/meet/index.html"
+  }
+];
